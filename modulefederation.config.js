f5c1398894638dca7597870ebad47c40f6324c0e
anttorre <antonio.torre@everis.com>
2022-03-21 12:54:37 +0100

    module sharing configured

--- modulefederation.config.js
+++ modulefederation.config.js
@@ -1,5 +1,12 @@
-const { dependencies } = require('./package.json');
+const { appDependencies } = require('./package.json');
 const commonDependencies = require('@pagopa/selfcare-common-frontend/package.json').dependencies;
+const muiItaliaDependencies = require('@pagopa/mui-italia/package.json').dependencies;
+
+const dependencies = {
+  ...muiItaliaDependencies,
+  ...commonDependencies,
+  ...appDependencies,
+};
 
 module.exports = {
   name: 'selfcareUsers',
@@ -9,11 +16,17 @@ module.exports = {
   },
   filename: 'remoteEntry.js',
   shared: {
-    ...commonDependencies,
-    ...dependencies,
     '@pagopa/selfcare-common-frontend': {
       singleton: true,
       requiredVersion: dependencies['@pagopa/selfcare-common-frontend'],
+    },
+    '@pagopa/mui-italia': {
+      singleton: true,
+      requiredVersion: dependencies['@pagopa/mui-italia'],
+    },
+    '@pagopa/ts-commons': {
+      singleton: true,
+      requiredVersion: dependencies['@pagopa/ts-commons'],
     },
     react: {
       singleton: true,
@@ -22,6 +35,38 @@ module.exports = {
     'react-dom': {
       singleton: true,
       requiredVersion: dependencies['react-dom'],
+    },
+    'react-redux': {
+      singleton: true,
+      requiredVersion: dependencies['react-redux'],
+    },
+    'react-router-dom': {
+      singleton: true,
+      requiredVersion: dependencies['react-router-dom'],
+    },
+    '@mui/icons-material': {
+      singleton: true,
+      requiredVersion: dependencies['@mui/icons-material'],
+    },
+    '@mui/material': {
+      singleton: true,
+      requiredVersion: dependencies['@mui/material'],
+    },
+    '@mui/x-data-grid': {
+      singleton: true,
+      requiredVersion: dependencies['@mui/x-data-grid'],
+    },
+    '@mui/x-data-grid-generator': {
+      singleton: true,
+      requiredVersion: dependencies['@mui/x-data-grid-generator'],
+    },
+    i18next: {
+      singleton: true,
+      requiredVersion: dependencies['i18next'],
+    },
+    'react-i18next': {
+      singleton: true,
+      requiredVersion: dependencies['react-i18next'],
     },
   },
 };
