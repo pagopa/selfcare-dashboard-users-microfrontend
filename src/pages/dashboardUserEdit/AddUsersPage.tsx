b7ed6c95c8729665227a70386e5a639bb9b80f9e
loremedda <lorenzo.medda.sa@nttdata.com>
2022-07-06 15:31:57 +0200

    new UI addUserForm

--- src/pages/dashboardUserEdit/AddUsersPage.tsx
+++ src/pages/dashboardUserEdit/AddUsersPage.tsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import TitleBox from '@pagopa/selfcare-common-frontend/components/TitleBox';
 import { resolvePathVariables } from '@pagopa/selfcare-common-frontend/utils/routes-utils';
 import { useTranslation } from 'react-i18next';
+import { PeopleAlt } from '@mui/icons-material';
 import { Product } from '../../model/Product';
 import ProductNavigationBar from '../../components/ProductNavigationBar';
 import { DASHBOARD_USERS_ROUTES } from '../../routes';
@@ -20,15 +21,18 @@ function AddUsersPage({ party, activeProducts, productsRolesMap }: Props) {
   const { t } = useTranslation();
   const history = useHistory();
 
+  const goBack = () =>
+    history.push(
+      resolvePathVariables(DASHBOARD_USERS_ROUTES.PARTY_USERS.subRoutes.MAIN.path, {
+        partyId: party.partyId,
+      })
+    );
+
   const paths = [
     {
       description: t('userPagesPath.detailRedirect'),
-      onClick: () =>
-        history.push(
-          resolvePathVariables(DASHBOARD_USERS_ROUTES.PARTY_USERS.subRoutes.MAIN.path, {
-            partyId: party.partyId,
-          })
-        ),
+      icon: PeopleAlt,
+      onClick: goBack,
     },
     {
       description: t('userPagesPath.addUser'),
@@ -39,39 +43,43 @@ function AddUsersPage({ party, activeProducts, productsRolesMap }: Props) {
     <Grid
       container
       alignItems={'center'}
-      px={2}
-      mt={10}
-      sx={{ width: '985px', backgroundColor: 'transparent !important' }}
+      px={3}
+      mt={4}
+      sx={{ width: '100%', backgroundColor: 'transparent !important' }}
     >
-      <Grid item xs={12} mb={3}>
-        <ProductNavigationBar paths={paths} />
-      </Grid>
-      <Grid item xs={12} mb={9}>
-        <TitleBox
-          variantTitle="h4"
-          variantSubTitle="body1"
-          title={t('userEdit.addForm.title')}
-          subTitle={t('userEdit.addForm.subTitle')}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <AddUserForm
-          party={party}
-          products={activeProducts}
-          productsRolesMap={productsRolesMap}
-          initialFormData={{
-            taxCode: '',
-            name: '',
-            surname: '',
-            email: '',
-            confirmEmail: '',
-            certifiedMail: false,
-            certifiedName: false,
-            certifiedSurname: false,
-            productRoles: [],
-          }}
-          canEditRegistryData={true}
-        />
+      <Grid container item xs={8}>
+        <Grid item xs={12} mb={2}>
+          <ProductNavigationBar paths={paths} showBackComponent={true} goBack={goBack} />
+        </Grid>
+        <Grid item xs={12}>
+          <TitleBox
+            variantTitle="h4"
+            variantSubTitle="body1"
+            title={t('userEdit.addForm.title')}
+            subTitle={t('userEdit.addForm.subTitle')}
+            mbTitle={1}
+            mbSubTitle={5}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <AddUserForm
+            party={party}
+            products={activeProducts}
+            productsRolesMap={productsRolesMap}
+            initialFormData={{
+              taxCode: '',
+              name: '',
+              surname: '',
+              email: '',
+              confirmEmail: '',
+              certifiedMail: false,
+              certifiedName: false,
+              certifiedSurname: false,
+              productRoles: [],
+            }}
+            canEditRegistryData={true}
+          />
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/pages/dashboardUserEdit/AddUsersProductPage.tsx
+++ src/pages/dashboardUserEdit/AddUsersProductPage.tsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import TitleBox from '@pagopa/selfcare-common-frontend/components/TitleBox';
 import { resolvePathVariables } from '@pagopa/selfcare-common-frontend/utils/routes-utils';
 import { useTranslation } from 'react-i18next';
+import { PeopleAlt } from '@mui/icons-material';
 import { Product } from '../../model/Product';
 import ProductNavigationBar from '../../components/ProductNavigationBar';
 import { DASHBOARD_USERS_ROUTES } from '../../routes';
@@ -25,16 +26,19 @@ function AddUsersProductPage({ party, selectedProduct, activeProducts, productRo
     [selectedProduct.id]: productRolesList,
   };
 
+  const goBack = () =>
+    history.push(
+      resolvePathVariables(DASHBOARD_USERS_ROUTES.PARTY_PRODUCT_USERS.subRoutes.MAIN.path, {
+        partyId: party.partyId,
+        productId: selectedProduct.id,
+      })
+    );
+
   const paths = [
     {
       description: t('userPagesPath.detailRedirect'),
-      onClick: () =>
-        history.push(
-          resolvePathVariables(DASHBOARD_USERS_ROUTES.PARTY_PRODUCT_USERS.subRoutes.MAIN.path, {
-            partyId: party.partyId,
-            productId: selectedProduct.id,
-          })
-        ),
+      icon: PeopleAlt,
+      onClick: goBack,
     },
     {
       description: t('userPagesPath.addUser'),
@@ -45,40 +49,49 @@ function AddUsersProductPage({ party, selectedProduct, activeProducts, productRo
     <Grid
       container
       alignItems={'center'}
-      px={2}
-      mt={10}
-      sx={{ width: '985px', backgroundColor: 'transparent !important' }}
+      px={3}
+      mt={4}
+      sx={{ width: '100%', backgroundColor: 'transparent !important' }}
     >
-      <Grid item xs={12} mb={3}>
-        <ProductNavigationBar selectedProduct={selectedProduct} paths={paths} />
-      </Grid>
-      <Grid item xs={12} mb={9}>
-        <TitleBox
-          variantTitle="h4"
-          variantSubTitle="body1"
-          title={t('userEdit.addForm.title')}
-          subTitle={t('userEdit.addForm.subTitle')}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <AddUserForm
-          party={party}
-          selectedProduct={selectedProduct}
-          products={activeProducts}
-          productsRolesMap={productsRolesMap}
-          canEditRegistryData={true}
-          initialFormData={{
-            taxCode: '',
-            name: '',
-            surname: '',
-            email: '',
-            confirmEmail: '',
-            productRoles: [],
-            certifiedMail: false,
-            certifiedName: false,
-            certifiedSurname: false,
-          }}
-        />
+      <Grid container item xs={8}>
+        <Grid item xs={12} mb={2}>
+          <ProductNavigationBar
+            selectedProduct={selectedProduct}
+            paths={paths}
+            showBackComponent={true}
+            goBack={goBack}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TitleBox
+            variantTitle="h4"
+            variantSubTitle="body1"
+            title={t('userEdit.addForm.title')}
+            subTitle={t('userEdit.addForm.subTitle')}
+            mbTitle={1}
+            mbSubTitle={5}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <AddUserForm
+            party={party}
+            selectedProduct={selectedProduct}
+            products={activeProducts}
+            productsRolesMap={productsRolesMap}
+            canEditRegistryData={true}
+            initialFormData={{
+              taxCode: '',
+              name: '',
+              surname: '',
+              email: '',
+              confirmEmail: '',
+              productRoles: [],
+              certifiedMail: false,
+              certifiedName: false,
+              certifiedSurname: false,
+            }}
+          />
+        </Grid>
       </Grid>
     </Grid>
   );
