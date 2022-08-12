7c6239411493b8299ec5bc1545c9a15259f1bd77
USERSAD\esignore <elena.signore@nttdata.com>
2022-08-12 15:54:33 +0200

    SELC-1476 - added placement and arrow in Tooltip

--- src/pages/dashboardUserDetail/userDetailPage/components/UserProductDetail.tsx
+++ src/pages/dashboardUserDetail/userDetailPage/components/UserProductDetail.tsx
@@ -87,7 +87,7 @@ export default function UserProductDetail({
 
           {product.authorized === false && !partyUser.isCurrentUser ? (
             <Grid item xs={5} display="flex" alignItems="center" justifyContent="flex-end">
-              <Tooltip title={t('userDetail.infoIcon')}>
+              <Tooltip title={t('userDetail.infoIcon')} placement="top" arrow={true}>
                 <InfoOutlinedIcon sx={{ cursor: 'pointer' }} color="primary" />
               </Tooltip>
             </Grid>
