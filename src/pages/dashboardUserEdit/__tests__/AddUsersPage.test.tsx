38a00704b241e0c8bc2daec9291659161d6bbd17
loremedda <lorenzo.medda.sa@nttdata.com>
2022-09-08 18:05:23 +0200

    Fixed unitTest

--- src/pages/dashboardUserEdit/__tests__/AddUsersPage.test.tsx
+++ src/pages/dashboardUserEdit/__tests__/AddUsersPage.test.tsx
@@ -21,7 +21,7 @@ jest.mock('../../../services/usersService');
 jest.setTimeout(6000);
 
 const fieldsValue = {
-  taxCode: 'AAAAAA11A11A234S',
+  taxCode: 'RSSFNC92B44C123K',
   name: 'franco',
   surname: 'rossi',
   email: 'NAME@SURNAME.COM',
