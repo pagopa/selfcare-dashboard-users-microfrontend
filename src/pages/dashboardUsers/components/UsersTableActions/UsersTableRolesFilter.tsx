000c3c15de9291ec49664c0c8d49932a57594fce
loremedda <lorenzo.medda.sa@nttdata.com>
2022-10-27 18:13:37 +0200

    Removed refuse

--- src/pages/dashboardUsers/components/UsersTableActions/UsersTableRolesFilter.tsx
+++ src/pages/dashboardUsers/components/UsersTableActions/UsersTableRolesFilter.tsx
@@ -261,7 +261,6 @@ export default function UsersTableRolesFilter({
                     <MenuItem
                       onClick={() => handleUserRole(isSelected, selcGroup, selcRole)}
                       key={selcRole}
-                      data-testid={`userRole: ${selcRole}`}
                     >
                       <FormControlLabel
                         sx={{
