bda1ad43d38e9a4d565cc90c10d583132bdd9b5a
OraldoDoci <oraldo.doci@nttdata.com>
2026-07-08 16:08:09 +0200

    remove comment

--- src/pages/addUserFlow/addUser/components/AddUserForm/AddUserForm.tsx
+++ src/pages/addUserFlow/addUser/components/AddUserForm/AddUserForm.tsx
@@ -304,7 +304,6 @@ export default function AddUserForm({
 
   useEffect(() => {
     if (userProduct) {
-      // setProductRoles(productsRolesMap[userProduct.id]);
 
       const fullRolesList = productsRolesMap[userProduct.id];
       const filteredRolesList = productRoles2ProductRolesList(
